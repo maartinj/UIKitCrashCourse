c06e0daffd7ed829a9c6dd7448da22889f4c41a6
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-03-11 21:03:12 +0100

    Building components in UIKit

--- UIKitCrashCourse/PeopleViewController.swift
+++ UIKitCrashCourse/PeopleViewController.swift
@@ -23,6 +23,41 @@ class PeopleViewController: UIViewController {
         btn.addTarget(self, action: #selector(didTapSubscribe), for: .touchUpInside)
         return btn
     }()
+    
+    private lazy var personContainerVw: UIView = {
+        
+        let vw = UIView()
+        vw.backgroundColor = UIColor.gray.withAlphaComponent(0.1)
+        vw.translatesAutoresizingMaskIntoConstraints = false
+        return vw
+    }()
+    
+    private lazy var nameLbl: UILabel = {
+        
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Billy Bob"
+        lbl.font = .systemFont(ofSize: 24, weight: .bold)
+        return lbl
+    }()
+    
+    private lazy var emailLbl: UILabel = {
+        
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "billy.bob@gmail.com"
+        lbl.font = .systemFont(ofSize: 15, weight: .regular)
+        return lbl
+    }()
+    
+    private lazy var personStackVw: UIStackView = {
+        
+        let vw = UIStackView()
+        vw.translatesAutoresizingMaskIntoConstraints = false
+        vw.axis = .vertical
+        vw.spacing = 8
+        return vw
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,11 +72,23 @@ private extension PeopleViewController {
         
         self.view.backgroundColor = .white
         
-        self.view.addSubview(subscribeBtn)
+        self.view.addSubview(personContainerVw)
+        
+        personContainerVw.addSubview(personStackVw)
+        
+        personStackVw.addArrangedSubview(nameLbl)
+        personStackVw.addArrangedSubview(emailLbl)
+        personStackVw.addArrangedSubview(subscribeBtn)
         
         NSLayoutConstraint.activate([
-            subscribeBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            subscribeBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            personContainerVw.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            personContainerVw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            personContainerVw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+            personStackVw.topAnchor.constraint(equalTo: personContainerVw.topAnchor, constant: 8),
+            personStackVw.bottomAnchor.constraint(equalTo: personContainerVw.bottomAnchor, constant: -8),
+            personStackVw.leadingAnchor.constraint(equalTo: personContainerVw.leadingAnchor, constant: 8),
+            personStackVw.trailingAnchor.constraint(equalTo: personContainerVw.trailingAnchor, constant: -8)
         ])
     }
     
